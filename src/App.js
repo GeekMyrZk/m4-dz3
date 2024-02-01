b41c6bb47d7f5f0b395d2f88d4b1f1cf113bd2f2
GeekMyrZk <dlaprogramminga@gmail.com>
2024-02-02 02:44:09 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [newTaskText, setNewTaskText] = useState('')
+    const [tasks, setTasks] = useState([])
+    const AddTask = () => {
+        if (newTaskText.trim() !== '') {
+            setTasks([newTaskText, ...tasks]);
+            setNewTaskText('');
+        }
+    };
+
+    return (
+      <>
+        <div className='toDo'
+             style={{
+                 textAlign: 'center',
+                 margin: '50px auto',
+                 width: '300px',
+                 padding: '20px',
+                 border: '1px solid #ccc',
+                 borderRadius: '8px',
+                 boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'}}>
+          <h1 style={{color: '#333'}}>Your tasks</h1>
+          <input
+              style={{
+                  padding: '8px',
+                  marginBottom: '10px',
+                  width: '100%',
+                  boxSizing: 'border-box'
+              }}
+              type="text"
+              value={newTaskText}
+              placeholder={'Enter your task'}
+              onChange={(e)=> setNewTaskText(e.target.value)}
+          />
+          <button onClick={AddTask}
+                  style={{
+                      backgroundColor: '#4caf50',
+                      marginLeft: '15px',
+                      padding: '8px 15px',
+                      border: 'none',
+                      borderRadius: '4px',
+                      cursor: "pointer",}}
+                  onMouseEnter={event => event.target.style.backgroundColor = '#439a47'}
+                  onMouseLeave={event => event.target.style.backgroundColor = '#4caf50'}
+          >Add task</button>
+
+          <ul className="tasks"
+              style={{
+                  listStyle: 'none',
+                  padding: '0',
+                  backgroundColor: '#94c979',
+                  borderRadius: '4px'}}
+          >{tasks.map((task, index) => (
+              <li style={{
+                  marginBottom: '5px',
+                  padding: '8px',
+                  borderRadius: '4px',
+                  boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)'
+              }}
+              key={index}>{task}</li>
+              ))}
+          </ul>
+        </div>
+      </>
+    );
 }
 
 export default App;
